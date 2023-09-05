80b9ec515938df821f8a49e183b9ea3a8e207363
VedMarolia <118649989+VedMarolia@users.noreply.github.com>
2023-09-05 17:36:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+noseX = 0
+noseY = 0
+rightWristX = 0
+leftWristX = 0
+difference = 0
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(550 , 500)
+    canvas.position(700 , 100)
+    
+    video = createCapture(VIDEO)
+    video.size(550 , 500)
+
+    poseNet = ml5.poseNet(video , modelLoaded)
+    poseNet.on("pose" , gotPoses)
+}
+
+function draw(){
+    background("gray")
+    fill("blue")
+    stroke("red")
+    rect(noseX , noseY , difference , difference)
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded")
+}
+
+function gotPoses(error,result){
+
+    if(error){
+        console.log(error)
+    }
+
+    else{
+        console.log(result)
+        
+        noseX = result[0].pose.nose.x;
+        noseY = result[0].pose.nose.y;
+
+        rightWristX = result[0].pose.rightWrist.x;
+        leftWristX = result[0].pose.leftWrist.x;
+
+        difference = Math.floor(rightWristX - leftWristX)
+    }
+}
